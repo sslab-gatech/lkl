6643c2ea058820d03498c0cda4cd7018c36f0b1f
Sanidhya Kashyap <sanidhya@gatech.edu>
2019-01-14 07:24:26 -0500

    fix virtio to compile with c++ program

--- include/uapi/linux/virtio_ring.h
+++ include/uapi/linux/virtio_ring.h
@@ -144,9 +144,9 @@ static inline void vring_init(struct vring *vr, unsigned int num, void *p,
 			      unsigned long align)
 {
 	vr->num = num;
-	vr->desc = p;
-	vr->avail = p + num*sizeof(struct vring_desc);
-	vr->used = (void *)(((uintptr_t)&vr->avail->ring[num] + sizeof(__virtio16)
+	vr->desc = (struct vring_desc*)p;
+	vr->avail = (struct vring_avail*)((char *)p + num*sizeof(struct vring_desc));
+	vr->used = (struct vring_used*)(((uintptr_t)&vr->avail->ring[num] + sizeof(__virtio16)
 		+ align-1) & ~(align - 1));
 }
 
